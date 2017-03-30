bd593d2e4cf3d7fc74cfb7aa252ae0a8e8ee5ca7
Ryan Riley <ryanriley@live.com>
2017-03-30 12:26:06 -0500

    react-handsontable.d.ts works locally

--- src/react-handsontable.d.ts
+++ src/react-handsontable.d.ts
@@ -3,41 +3,40 @@
 // Definitions by: Ryan Riley <https://github.com/panesofglass/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped/
 
-import * as React from "react"
-import * as Handsontable from "handsontable"
-
 declare namespace HotTable {
 
     export interface Props extends ht.Options {
         root?: string
         settings?: ht.Options
     }
+
+    /**
+     * A Handsontable-ReactJS wrapper.
+     *
+     * To implement, use the `HotTable` tag with properties corresponding to Handsontable options.
+     * For example:
+     *
+     * ```js
+     * <HotTable root="hot" data={dataObject} contextMenu={true} colHeaders={true} width={600} height={300} stretchH="all" />
+     *
+     * // is analogous to
+     * let hot = new Handsontable(document.getElementById('hot'), {
+     *    data: dataObject,
+     *    contextMenu: true,
+     *    colHeaders: true,
+     *    width: 600
+     *    height: 300
+     * });
+     *
+     * ```
+     *
+     * @class HotTable
+     */
+    export class HotTable extends React.Component<Props, {}> {
+
+    }
 }
 
-/**
- * A Handsontable-ReactJS wrapper.
- *
- * To implement, use the `HotTable` tag with properties corresponding to Handsontable options.
- * For example:
- *
- * ```js
- * <HotTable root="hot" data={dataObject} contextMenu={true} colHeaders={true} width={600} height={300} stretchH="all" />
- *
- * // is analogous to
- * let hot = new Handsontable(document.getElementById('hot'), {
- *    data: dataObject,
- *    contextMenu: true,
- *    colHeaders: true,
- *    width: 600
- *    height: 300
- * });
- *
- * ```
- *
- * @class HotTable
- */
-declare class HotTable extends React.Component<HotTable.Props, {}> {
-
+declare module "react-handsontable" {
+    export default HotTable.HotTable;
 }
-
-export default HotTable;
